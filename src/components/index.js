429f5b5a221f501256e1a7661099e1207cc1d0d5
rodik <i6@bk.ru>
2016-02-15 17:00:59 +0200

    removing CounterButton component (left from hot example)

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
-export CounterButton from './CounterButton/CounterButton';
 export GithubButton from './GithubButton/GithubButton';
-export WidgetForm from './WidgetForm/WidgetForm';
 export LoginForm from './LoginForm/LoginForm';
 export Modal from './Modal/Modal';
 export Toast from './Toast/Toast';
