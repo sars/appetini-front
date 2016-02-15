70a53613c8d095f83fdba2ec88178479b0fe1539
rodik <i6@bk.ru>
2016-02-15 16:59:41 +0200

    removing InfoBar and MiniInfoBar components (left from hot example)

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,5 @@
-/**
- *  Point of contact for component modules
- *
- *  ie: import { CounterButton, InfoBar } from 'components';
- *
- */
-
 export CounterButton from './CounterButton/CounterButton';
 export GithubButton from './GithubButton/GithubButton';
-export InfoBar from './InfoBar/InfoBar';
-export MiniInfoBar from './MiniInfoBar/MiniInfoBar';
 export WidgetForm from './WidgetForm/WidgetForm';
 export LoginForm from './LoginForm/LoginForm';
 export Modal from './Modal/Modal';
