9c2eb32a45b27191c3a402b438f678de8ef93b68
rodik <i6@bk.ru>
2016-03-28 00:01:03 +0300

    small fix for deliveryTime (hours should be represent by 2 digits

--- src/components/DeliveryTimeDropdown/DeliveryTimeDropdown.js
+++ src/components/DeliveryTimeDropdown/DeliveryTimeDropdown.js
@@ -3,7 +3,7 @@ import Dropdown from 'components/Dropdown/Dropdown';
 
 const deliveryTimeOptions = [
   { label: 'Время' },
-  { value: '9:30Z', label: '12:30 - 13:00' }, // value in UTC
+  { value: '09:30Z', label: '12:30 - 13:00' }, // value in UTC
   { value: '10:30Z', label: '13:30 - 14:00' }  // value in UTC
 ];
 
