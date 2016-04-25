dd634058dff9856d9eca2ff6da19238711715f2e
rodik <i6@bk.ru>
2016-04-25 19:08:26 +0300

    ready_by_time options changed

--- src/components/DeliveryTimeDropdown/DeliveryTimeDropdown.js
+++ src/components/DeliveryTimeDropdown/DeliveryTimeDropdown.js
@@ -4,7 +4,8 @@ import Dropdown from 'components/Dropdown/Dropdown';
 const deliveryTimeOptions = [
   { label: 'Время' },
   { value: '09:30Z', label: '12:30 - 13:00' }, // value in UTC
-  { value: '10:30Z', label: '13:30 - 14:00' }  // value in UTC
+  { value: '10:00Z', label: '13:00 - 13:30' },  // value in UTC
+  { value: '15:00Z', label: '18:00 - 18:30' }  // value in UTC
 ];
 
 const DeliveryTimeDropdown = ({...other}) => {
